8594add9f5da9f06971ed085e905b2f960b834db
Glitch (excessive-termite) <none>
2019-11-09 17:21:17 +0000

    🏚😄 Checkpoint ./routes/api.js:7351991/412

--- routes/api.js
+++ routes/api.js
@@ -18,24 +18,24 @@ module.exports = function (app) {
 
   app.route('/api/issues/:project')
   
-    .get(function (req, res){
-      var project = req.params.project;
-      
-    })
-    
-    .post(function (req, res){
-      var project = req.params.project;
-      
-    })
-    
-    .put(function (req, res){
-      var project = req.params.project;
-      
-    })
-    
-    .delete(function (req, res){
-      var project = req.params.project;
-      
-    });
+  .get(function (req, res){
+    var project = req.params.project;
+
+  })
+
+  .post(function (req, res){
+    var project = req.params.project;
+
+  })
+
+  .put(function (req, res){
+    var project = req.params.project;
+
+  })
+
+  .delete(function (req, res){
+    var project = req.params.project;
+
+  });
     
 };
