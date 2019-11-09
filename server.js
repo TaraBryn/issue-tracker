4a5ada50225ca87f599c54c2153cc4d2ccff922e
Glitch (excessive-termite) <none>
2019-11-09 22:47:27 +0000

    🕰🏎 Checkpoint ./server.js:7351991/3167

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
-const helmet = require('helmet');
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
@@ -15,9 +14,6 @@ var app = express();
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
-app.use(helmet());
-app.use(helmet.contentSecurityPolicy({directives: {'defaultSrc': ["'self'"]}}));
-
 
 
 
