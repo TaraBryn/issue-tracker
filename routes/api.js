cb63794c232745c072f6a96a52f5e5aad325ff37
Glitch (excessive-termite) <none>
2019-11-13 18:47:39 +0000

    🚤🍉 Checkpoint ./routes/api.js:7351991/3761

--- routes/api.js
+++ routes/api.js
@@ -54,53 +54,60 @@ module.exports = function (app, db) {
   })
 
   .put(function (req, res){
+    console.log('test')
     var project = req.params.project;
-    
-    var _id = req.body._id;
-    
-    var issue_title = req.body.issue_title == '';
-    var issue_text = req.body.issue_text;
-    var created_by = req.body.created_by;
-    var assigned_to = req.body.assigned_to;
-    var status_text = req.body.status_text;
-    
-    if (!(issue_title || issue_text || created_by || assigned_to || status_text))
+    var {_id, issue_title, issue_text, created_by, assigned_to, status_text, open} = req.body;
+    if(!(issue_title || issue_text || created_by || assigned_to || status_text || open))
       return 'no updated field sent';
-    
-    /*db.collection('projects').find(
-    {'issues._id': new ObjectId(_id)},
-    (err, doc) => {
-      if (err) return 'could not update' + _id;
-      try{
-        var project_id = doc[0]._id;
-        var issue = doc[0].issues.filter(e=>e._id==_id)[0];
-        
-        issue_title = issue_title || issue.issue_title;
-        issue_text = issue_text || issue.issue_text;
-        created_by = created_by || issue.created_by;
-        assigned_to = assigned_to || issue.assigned_to;
-        status_text = status_text || issue.status_text;
-        
-        db.findAndModify({
-          query: {
-            _id: new ObjectId(project_id),
-            'issues': {$elemMatch: {_id: new ObjectId(_id)}}
-          },
-          update: {
-            $set: {
-              'issues.$issue_title': issue_title,
-              'issues.$issue_text': issue_text,
-              'issues.$created_by': created_by,
-              'issues.$assigned_to': assigned_to,
-              'issues.$status.text': status_text
+    db.collection('projects').find(
+      {'issues._id': ObjectId(_id)},
+      function(err, doc){
+        if (err) console.log('error: ', err);
+        console.log(doc.toArray()[0])
+        //doc.forEach((e, i)=>console.log(i, e));
+        try{
+          var project_id = doc[0]._id;
+          var issue = doc[0].issues.filter(e=>e._id==_id)[0];
+          
+          issue_title = issue_title || issue.issue_title;
+          issue_text = issue_text || issue.issue_text;
+          created_by = created_by || issue.created_by;
+          assigned_to = assigned_to || issue.assigned_to;
+          status_text = status_text || issue.status.text;
+          console.log(issue_title, issue_text);
+          db.findAndModify({
+            query: {
+              _id: ObjectId(project_id),
+              'issues': {$elemMatch: {_id: ObjectId(_id)}}
+            },
+            update: {
+              $set: {
+                'issues.$.issue_title': issue_title,
+                'issues.$.issue_text': issue_text,
+                'issues.$.created_by': created_by,
+                'issues.$.assigned_to': assigned_to,
+                'issues.$.status_text': status_text,
+                'issues.$.open': !open,
+                'issues.$.date_updated': new Date()
+                /*issues: {
+                  issue_title,
+                  issue_text,
+                  created_by,
+                  assigned_to,
+                  status_text,
+                  open: !open,
+                  date_updated:new Date()
+                }*/
+              }
             }
-          }
-        })
-        
+          })
+        }
+        catch(e){
+          console.log('try error: ', e)
+          return 'could not update ' + _id
+        }
       }
-      catch(e){return 'could not update' + _id}
-    })*/
-    return 'successfully updated';
+    )
   })
 
   .delete(function (req, res){
